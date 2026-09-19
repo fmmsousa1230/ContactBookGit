653a61233f0cdf9dec5a8ce59495ebf6f2045b16
Francisco <fmm.sousa@campus.fct.unl.pt>
2026-09-19 18:06:26 +0100

    Added public constants "GN" & "EP"

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
+    public static final String GET_NUMBER  = "GN";
+    public static final String EQUAL_PHONE  = "EP";
     public static final String QUIT           = "Q";
 
     //Constantes que definem as mensagens para o utilizador
